ba12fc8172439d199f1c76d420881f00c53e1f82
gor-asatryan96 <asatryan.gor96@gmail.com>
2021-07-15 16:39:19 +0400

    activePopup duck

---
+++ src/redux/ducks/popupDuck.js
@@ -0,0 +1,16 @@
+import { createAction, createReducer } from "../../helpers/redux"
+
+// ACTION TYPES
+const SET_ACTIVE_POPUP = "SET_ACTIVE_POPUP";
+const RESET_POPUP = "RESET_POPUP";
+
+
+// ACTIONS
+export const setActivePopup = createAction(SET_ACTIVE_POPUP);
+export const resetPopup = createAction(RESET_POPUP);
+
+// REDUCER
+export const activePopup = createReducer(null, (_, { value }) => ({
+    [SET_ACTIVE_POPUP]: () => value,
+    [RESET_POPUP]: () => null,
+}));
